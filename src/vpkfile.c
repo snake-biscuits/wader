73c819663f7c5f217b29303367aca71d6ebc2ee2
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2022-05-27 17:34:08 +1000

    Cleaning up vpk prototype

--- src/vpkfile.c
+++ src/vpkfile.c
@@ -1,39 +1,90 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "vpkfile.h"
 
 
 #ifdef VPK_MAIN
-int main(int argc, char* argv[]) {
-    int               i;
-    FILE             *vpk;
-    ValveVpkHeader_t  vpk_header;
+void print_help() {
+  printf("Usage: vpk [OPTIONS] VPKFILE...\n");
+  printf("  -h    print help and exit\n");
+};
 
-    if (argc == 1) {
-        printf("Usage: vpk VPKFILE...\n");
-        return 0;
-    }
+
+int main(int argc, char* argv[]) {
+    int              i;
+    bool             respawn_format;
+    FILE            *vpk;
+    VpkId_t          vpk_id;
+    VpkHeaderv2_0_t  vpk_header_valve;
+    VpkHeaderv2_3_t  vpk_header_respawn;
+
+    respawn_format = false;
+
+    goto end_table_jmp;
+args_error_jmp:
+    fprintf(stderr, "invalid argument: %s\n", argv[i]);
+help_jmp:
+    print_help();
+    return 0;
+end_table_jmp:
+
+
+    // handle options
+    if (argc == 1) { goto help_jmp; }
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            if (strlen(argv[1]) != 2)
+                goto args_error_jmp;
+            switch (argv[i][1]) {
+                case 'h':  goto help_jmp;       break;
+                default:   goto args_error_jmp; break;
+            }
+        } else
+            break;
+    };
 
     #define VPKFILE_ASSERT(cond, msg)  FILE_ASSERT_BASE(cond, msg, argv[i], 1)
     #define READ_OK(dest, Type)        READ_OK_BASE(dest, Type, vpk)
 
-    for (i = 1; i < argc; i++) {
+    for (; i < argc; i++) {
         vpk = fopen(argv[i], "r");
         VPKFILE_ASSERT(!vpk, "Couldn't open %s!\n")
-        VPKFILE_ASSERT(READ_OK(vpk_header, ValveVpkHeader_t), "Failed to read %s header!\n")
-
         printf("Parsing: %s\n", argv[i]);
+        VPKFILE_ASSERT(READ_OK(vpk_id, VpkId_t), "%s is empty!\n")
+        VPKFILE_ASSERT(vpk_id.magic != VPK_MAGIC, "%s is not a known .vpk format!\n")
+        if (vpk_id.version.major == VALVE_VPK_MAJOR && vpk_id.version.minor == VALVE_VPK_MINOR)
+            respawn_format = false;
+        else if (vpk_id.version.major == RESPAWN_VPK_MAJOR && vpk_id.version.minor == RESPAWN_VPK_MINOR)
+            respawn_format = true;
+        else {
+            fprintf(stderr, "%s is not a known .vpk format (v%d.%d)\n", argv[i], vpk_id.version.major, vpk_id.version.minor);
+            return 1;
+        }
 
-        #define PRINT_ATTR(fmt, attr)  printf("%-22s = " fmt "\n", #attr, vpk_header.attr);
-        PRINT_ATTR("%08X", magic)
-        printf("%-22s = %d.%d\n", "version", vpk_header.version_major, vpk_header.version_minor);
-        PRINT_ATTR("%d", tree_length)
-        PRINT_ATTR("%d", embedded_chunk_length)
-        PRINT_ATTR("%d", chunk_hashes_length)
-        PRINT_ATTR("%d", self_hashes_length)
-        PRINT_ATTR("%d", signature_size)
-        #undef PRINT_ATTR
+        printf("%-22s = %08X\n",  "magic", vpk_id.magic);
+        printf("%-22s = %d.%d\n", "version", vpk_id.version.major, vpk_id.version.minor);
+        fseek(vpk, 0, SEEK_SET);
+        if (!respawn_format) {  // Valve format
+            VPKFILE_ASSERT(READ_OK(vpk_header_valve, VpkHeaderv2_0_t), "Failed to read %s header!\n")
+            #define PRINT_ATTR(fmt, attr)  printf("%-22s = " fmt "\n", #attr, vpk_header_valve.attr);
+            PRINT_ATTR("%d", tree_size)
+            PRINT_ATTR("%d", embedded_chunk_size)
+            PRINT_ATTR("%d", chunk_hashes_size)
+            PRINT_ATTR("%d", self_hashes_size)
+            PRINT_ATTR("%d", signature_size)
+            #undef PRINT_ATTR
+            // TODO: collect DirEntry tree & filenames
+        } else {  // Titanfall 1 format
+            VPKFILE_ASSERT(READ_OK(vpk_header_respawn, VpkHeaderv2_3_t), "Failed to read %s header!\n")
+            #define PRINT_ATTR(fmt, attr)  printf("%-22s = " fmt "\n", #attr, vpk_header_respawn.attr);
+            PRINT_ATTR("%d", tree_size)
+            PRINT_ATTR("%d", embedded_chunk_size)
+            #undef PRINT_ATTR
+            // TODO: collect DirEntry tree & filenames
+        }
 
         #undef READ_OK
         CLOSE_FILE(vpk, argv[i])
--- src/vpkfile.h
+++ src/vpkfile.h
@@ -19,57 +19,80 @@
 #define RESPAWN_VPK_MINOR  3
 
 
-typedef struct ValveVpkHeader {
-    uint32_t  magic;                  // VPK_MAGIC
-    uint16_t  version_major;          // VALVE_VPK_MAJOR (2)
-    uint16_t  version_minor;          // VALVE_VPK_MINOR (0)
-    uint32_t  tree_length;
-    uint32_t  embedded_chunk_length;  // 0 if "*_dir.vpk"
-    uint32_t  chunk_hashes_length;
-    uint32_t  self_hashes_length;     // 48 ?
+////////////////
+// Shared VPK //
+////////////////
+typedef struct VpkId {
+    uint32_t      magic;  // VPK_MAGIC
+    struct {
+        uint16_t  major;  // VALVE/RESPAWN_VPK_MAJOR (2/2)
+        uint16_t  minor;  // VALVE/RESPAWN_VPK_MINOR (0/3)
+    } version;
+} VpkId_t;
+
+
+// MASTER STRUCTURE:
+// VpkHeaderv2_x  (id & structure sizes)
+// FilenameTree
+// -- szExtension
+// -- szDirectory
+// -- szFilename
+// NOTE: get 3 strings first, then any number (1..3) in a block
+//       1 for new filename in dir, 2 for new dir, 3 for new ext
+// -- MetaData / DirEntry (per-file)
+// EmbeddedChunk (concatenate "*_%03d.vpk" if "*_dir.vpk" ?)
+
+// "*_dir.vpk" are just the header & tree
+
+
+///////////////
+// Valve VPK //
+///////////////
+typedef struct VpkHeaderv2_0 {
+    VpkId_t   id;
+    uint32_t  tree_size;            // DirEntry / filename tree
+    uint32_t  embedded_chunk_size;  // 0 if "*_dir.vpk"
+    uint32_t  chunk_hashes_size;
+    uint32_t  self_hashes_size;     // 48 ?
     uint32_t  signature_size;
-    // Next comes the tree...
-} ValveVpkHeader_t;
+} VpkHeaderv2_0_t;
 
 
-typedef struct ValveVpkChunkMetadata {  // embed chunk structures
-    uint32_t  checksum;        // crc32
-    uint16_t  preload_length;  // 0x0000 ?
-    uint16_t  archive_index;   // 0x7FFF ?
+typedef struct VpkDirEntryv2_0 {
+    uint32_t  checksum;        // crc32 of data (bitmasked down to 32 bits)
+    uint16_t  preload_size;    // almost always 0x0000
+    uint16_t  archive_index;   // almost always 0x7FFF
     uint32_t  archive_offset;  // file offset - tree & header sizes
-    uint32_t  file_length;
-    uint16_t  suffix;          // 0xFFFF ? block terminator?
-} ValveVpkChunkMetadata;
+    uint32_t  file_size;
+    uint16_t  suffix;          // always 0xFFFF (ensure read alignment)
+} VpkDirEntryv2_0_t;
 
 
 /////////////////
 // Respawn VPK //
 /////////////////
-typedef struct RespawnVpkHeader {
-    uint32_t  magic;          // VPK_MAGIC
-    uint16_t  version_major;  // RESPAWN_VPK_MAJOR (2)
-    uint16_t  version_minor;  // RESPAWN_VPK_MINOR (3)
-    uint32_t  directory_size;
-    uint32_t  embedded_chunk_size;
-    // uint32_t  self_hashes_size;
-    // uint32_t  signature_size;
-} RespawnVpkHeader_t;
+typedef struct VpkHeaderv2_3 {
+    VpkId_t   id;
+    uint32_t  tree_size;            // DirEntry / filename tree
+    uint32_t  embedded_chunk_size;  // 0 for '*_dir.vpk'
+} VpkHeaderv2_3_t;
 
 
-typedef struct RespawnVpkDirEntry {
-    uint32_t  flags_1;
+typedef struct VpkDirEntryv2_3 {
+    uint32_t  flags_1;    // what flags? what do they affect?
     uint16_t  flags_2;
     uint64_t  offset;
     uint64_t  disk_size;  // compressed
     uint64_t  size;       // decompressed (should be under 0x100000?)
-} RespawnVpkDirEntry_t;
+} VpkDirEntryv2_3_t;
 
 
 typedef struct RespawnVpkDirEntryBlock {
     uint32_t  checksum;
-    uint16_t  length;
+    uint16_t  size;
     uint16_t  file_index;
     // DirEntry, uint16_t, DirEntry, uint16_t; until uint16_t == 0xFFFF;
 } RespawnVpkDirEntryBlock_t;
+
 
 #endif
